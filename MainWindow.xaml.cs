124256bd12f646ab6d5a76c82a72d02582e5ac36
ejaszczuq <ewelina.jaszczuk13006@gmail.com>
2023-12-13 21:02:30 +0100

    start project and generate an array of numbers to sort

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sorting_algorithms
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int[] numbers = new int[10];
+
+                Random random = new Random();
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = random.Next(100);
+                }
+
+                string allNumbers = string.Join(", ", numbers);
+
+
+                tbNumbers.Text = allNumbers;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
+    
+
+
