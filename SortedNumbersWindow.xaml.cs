e9e596ff77e0c4997f015d53f4dde9424664fc24
ejaszczuq <ewelina.jaszczuk13006@gmail.com>
2023-12-20 21:45:33 +0100

    modified:   MainWindow.xaml

---
+++ SortedNumbersWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace sorting_algorithms
+{
+    public partial class SortedNumbersWindow : Window
+    {
+        public SortedNumbersWindow(string sortedNumbers, string algorithmName, long executionTime)
+        {
+            InitializeComponent();
+
+            tbSortedNumbers.Text = $"Sorted Numbers: {sortedNumbers}";
+            tbAlgorithmInfo.Text = $"Algorithm: {algorithmName}";
+            tbExecutionTime.Text = $"Execution Time: {executionTime / 1000.0:F6} seconds"; // Formatowanie czasu do 6 miejsc po przecinku
+        }
+    }
+}
