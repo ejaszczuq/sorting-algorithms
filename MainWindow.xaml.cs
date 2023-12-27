811ec78c64a795a0ffc65d59a8ed2cbbf91c93e3
ejaszczuq <ewelina.jaszczuk13006@gmail.com>
2023-12-27 02:54:23 +0100

    replace 'Windows' with 'Pages', add navigation logic, create DataStorage file, and move main code to StartPage

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,178 +1,19 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
+using sorting_algorithms;
 
 namespace sorting_algorithms
 {
     public partial class MainWindow : Window
     {
-        private int[] numbers = new int[10000];
-        private Random random = new Random();
+
 
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void GenerateButton_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    numbers[i] = random.Next(1000);
-                }
-
-                string allNumbers = string.Join(", ", numbers);
-
-                tbNumbers.Text = allNumbers;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        //QuickSort algorithm
-        private void QuickSortButton_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                int[] clonedNumbers = (int[])numbers.Clone();
-
-                var stopwatch = Stopwatch.StartNew();
-                QuickSort(clonedNumbers, 0, clonedNumbers.Length - 1);
-                stopwatch.Stop();
-
-                var sortedNumbersWindow = new SortedNumbersWindow(
-                    string.Join(", ", clonedNumbers),
-                    "QuickSort",
-                    stopwatch.ElapsedMilliseconds
-                );
-
-                sortedNumbersWindow.Show();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private void QuickSort(int[] arr, int first, int last)
-        {
-            if (first < last)
-            {
-                int partitionIndex = Partition(arr, first, last);
-
-                QuickSort(arr, first, partitionIndex - 1);
-                QuickSort(arr, partitionIndex + 1, last);
-            }
-        }
-
-        private int Partition(int[] arr, int first, int last)
-        {
-            int pivot = arr[last];
-            int i = first - 1;
-
-            for (int j = first; j < last; j++)
-            {
-                if (arr[j] < pivot)
-                {
-                    i++;
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-
-            int tempPivot = arr[i + 1];
-            arr[i + 1] = arr[last];
-            arr[last] = tempPivot;
-
-            return i + 1;
-        }
-
-        //BubbleSort algorithm
-        private void BubbleSortButton_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                int[] clonedNumbers = (int[])numbers.Clone();
-
-                var stopwatch = Stopwatch.StartNew();
-                BubbleSort(clonedNumbers);
-                stopwatch.Stop();
-
-                var sortedNumbersWindow = new SortedNumbersWindow(
-                    string.Join(", ", clonedNumbers),
-                    "BubbleSort",
-                    stopwatch.ElapsedMilliseconds
-                );
-
-                sortedNumbersWindow.Show();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private void BubbleSort(int[] arr)
-        {
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                for (int j = 0; j < arr.Length - 1 - i; j++)
-                {
-                    if (arr[j] > arr[j + 1])
-                    {
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
-                }
-            }
-        }
-
-        //InsertionSort algorithm
-        private void InsertionSortButton_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                int[] clonedNumbers = (int[])numbers.Clone();
-
-                var stopwatch = Stopwatch.StartNew();
-                InsertionSort(clonedNumbers);
-                stopwatch.Stop();
-
-                var sortedNumbersWindow = new SortedNumbersWindow(
-                    string.Join(", ", clonedNumbers),
-                    "InsertionSort",
-                    stopwatch.ElapsedMilliseconds
-                );
-
-                sortedNumbersWindow.Show();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private void InsertionSort(int[] arr)
-        {
-            for (int i = 1; i < arr.Length; i++)
-            {
-                int key = arr[i];
-                int j = i - 1;
-
-                while (j >= 0 && arr[j] > key)
-                {
-                    arr[j + 1] = arr[j];
-                    j--;
-                }
-
-                arr[j + 1] = key;
-            }
+            // Navigate(new MainContent());
         }
     }
 }
---
+++ SortedNumbersPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace sorting_algorithms
+{
+    public partial class SortedNumbersPage : Page
+    {
+        public SortedNumbersPage(string sortedNumbers, string algorithmInfo, long executionTime)
+        {
+            CustomInitializeComponent();
+
+            tbSortedNumbers.Text = sortedNumbers;
+            tbAlgorithmInfo.Text = algorithmInfo;
+            tbExecutionTime.Text = $"Execution Time: {executionTime} ms";
+        }
+
+        private void CustomInitializeComponent()
+        {
+            // Initialize components
+            tbSortedNumbers = new TextBox();
+            tbAlgorithmInfo = new TextBox();
+            tbExecutionTime = new TextBox();
+
+            Button back = new Button();
+            back.Command = NavigationCommands.BrowseBack;
+            back.Content = "Back";
+
+            // Settings for TextBoxes
+            tbSortedNumbers.IsReadOnly = true;
+            tbAlgorithmInfo.IsReadOnly = true;
+            tbExecutionTime.IsReadOnly = true;
+
+            // Add to the grid
+            Grid grid = new Grid();
+            grid.Background = new SolidColorBrush(Colors.White);
+            // Settings for the grid
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            // Settings for tbSortedNumbers
+            Grid.SetColumn(tbSortedNumbers, 0);
+            grid.Children.Add(tbSortedNumbers);
+
+            // Settings for tbAlgorithmInfo
+            Grid.SetColumn(tbAlgorithmInfo, 1);
+            grid.Children.Add(tbAlgorithmInfo);
+
+            // Settings for tbExecutionTime
+            Grid.SetColumn(tbExecutionTime, 2);
+            grid.Children.Add(tbExecutionTime);
+
+            // Settings for back button
+            Grid.SetColumn(back, 3);
+            grid.Children.Add(back);
+
+            Content = grid;
+        }
+
+        // Declarations of TextBoxes
+        private TextBox tbSortedNumbers;
+        private TextBox tbAlgorithmInfo;
+        private TextBox tbExecutionTime;
+    }
+}
